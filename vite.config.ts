c036589227537dd79ab05cb5adfe0ea2f9f9f2d3
Yuriy Bogomolov <yuriy.bogomolov@gmail.com>
2026-03-13 13:35:10 +0200

    Migrate to Vite 8

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 import topLevelAwait from 'vite-plugin-top-level-await';
-import wasm from 'vite-plugin-wasm';
 
 export default defineConfig({
   base: '/umc/',
-  plugins: [react(), wasm(), topLevelAwait()],
+  plugins: [react(), topLevelAwait()],
   worker: { format: 'es' },
   resolve: {
     alias: {
